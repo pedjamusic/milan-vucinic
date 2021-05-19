fb9fa452b31ceef2ca6bf79c764bbbb0ab56cd1f
Predrag Drljaca <hi@iampedja.com>
2021-05-19 11:33:16 +0200

    Netlify build issue, filename capitalization

---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import IconsList from './iconslist'
+
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <IconsList />
+      </ul>
+    </footer>
+  )
+}
