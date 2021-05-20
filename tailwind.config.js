0137b2ecd8b75008229a33354b0dd975b802db23
Predrag Drljaca <hi@iampedja.com>
2021-05-20 16:17:59 +0200

    Added safelist for dynamically created classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx,md,mdx,html}',
-    './public/**/*.html'
-  ],
+  purge: {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx,md,mdx,html}',
+      './public/**/*.html',
+    ],
+    options: {
+      safelist: ['grid-cols-1', 'grid-cols-2', 'grid-cols-3', 'grid-cols-4', 'grid-cols-5']
+    }
+  },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
